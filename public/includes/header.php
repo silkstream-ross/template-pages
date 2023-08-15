7ee964b897a8fe4cd6d16dc9c8e7df2148958270
Ross Wallings <ross@silkstream.net>
2023-08-15 16:47:38 +0100

    added header, banner image and started adding featured events, added basic structure for rest of page

---
+++ public/includes/header.php
@@ -0,0 +1,20 @@
+<header>
+    <nav class="main-nav">
+        <div class="main-nav-links">
+            <img class="main-nav-link site-logo" src="/images/Go Privilege Logo.png"  height="80">
+            <a class="main-nav-link">Events<b></b></a>
+            <a class="main-nav-link">Travel</a>
+            <a class="main-nav-link">About</a>
+            <a class="main-nav-link">Contact</a>
+            <a class="main-nav-link nav-log-in" href="#"><img src="/images/Profile Icon.png" width="50">Log In</a>
+        </div>
+        <div class="main-nav-banner">
+            <img class="banner-image" src="/images/banner-test.jpg">
+            <div class="banner-content">
+                <div class="banner-heading"><h2>VIP Luxury Experiences</h2></div>
+                <div class="banner-links"><a class="banner-link">Events</a><a class="banner-link">Travel</a></div>
+                <div class="banner-switch-links"><a class="banner-switch-link"></a><a class="banner-switch-link"></a></div>
+            </div>
+        </div>
+    </nav>
+</header>
--- public/index.php
+++ public/index.php
@@ -9,17 +9,93 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>template pages</title>
     <link rel="stylesheet" href="dist/css/default.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sen&display=swap" rel="stylesheet">
 </head>
 <body>
-<header>
-    <nav class="main-nav">
-        <a class="main-nav-link site-logo"></a>
-        <a class="main-nav-link">Events</a>
-        <a class="main-nav-link">Travel</a>
-        <a class="main-nav-link">About</a>
-        <a class="main-nav-link">Contact</a>
-        <a class="main-nav-link nav-log-in">Log In</a>
-    </nav>
-</header>
+<?php
+require_once (__DIR__.'/includes/header.php');
+?>
+<div class="main-page-content">
+    <div class="main-page-featured-events">
+        <div class="featured-events-heading">
+            <h2 class="section-heading">FEATURED EVENTS</h2>
+            <p class="section-subheading">VIP Access to Sold Out Events</p>
+        </div>
+        <div class="featured-events-container">
+            <div class="featured-events-rows">
+                <div class="featured-events-item">
+                    <img class="featured-event-img" src="/images/events/Royal Ascot 2024.jpg" width="400">
+                    <div class="featured-event-details">
+                        <h4 class="featured-event-category">Horse Racing</h4>
+                        <h3 class="featured-event-title">Royal Ascot 2024</h3>
+                        <p>Ascot Racecourse</p>
+                        <p>Tues 18th - Sat 22nd June 2024</p>
+                    </div>
+                </div>
+                <div class="featured-events-item-primary">
+                    <img class="featured-event-img" src="/images/events/Monaco Grand Prix 2024.jpg" height="400">
+                    <div class="featured-event-details">
+                        <h4 class="featured-event-category">Motorsport</h4>
+                        <h3 class="featured-event-title">2024 Formula 1 Monaco Grand Prix</h3>
+                        <p>Monte Carlo</p>
+                        <p>Fri 24th - Sun 26th May 2024</p>
+                    </div>
+                </div>
+                <div class="featured-events-item">
+                    <div class="featured-event-details">
+                        <img class="featured-event-img" src="/images/events/The 152nd Open.jpg" height="400">
+                        <h4 class="featured-event-category">Golf Major</h4>
+                        <h3 class="featured-event-title">The 152nd Open</h3>
+                        <p>Royal Troon Golf Course</p>
+                        <p>Wed 17th - Sun 21st July 2024</p>
+                    </div>
+                </div>
+                <div class="featured-events-item">
+                    <div class="featured-event-details">
+                        <h4 class="featured-event-category"></h4>
+                        <h3 class="featured-event-title"></h3>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="featured-events-item">
+                    <div class="featured-event-details">
+                        <h4 class="featured-event-category"></h4>
+                        <h3 class="featured-event-title"></h3>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="featured-events-end">
+
+        </div>
+    </div>
+    <div class="main-page-ambassador">
+        <div class="ambassador-content">
+            <div class="ambassador-image">
+
+            </div>
+            <div class="ambassador-text">
+
+            </div>
+        </div>
+    </div>
+    <div class="main-page-travel-services">
+
+    </div>
+    <div class="main-page-social-posts">
+
+    </div>
+    <div class="main-page-partners">
+
+    </div>
+</div>
+<?php
+//require_once (__DIR__.'/includes/footer.php');
+?>
 </body>
 </html>
